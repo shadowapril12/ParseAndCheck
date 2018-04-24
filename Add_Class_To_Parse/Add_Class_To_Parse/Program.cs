15ad64c8e21f32aa36a1fab9d9bd73d8552e2f3c
Nikolay <shadowapril@gmail.com>
2018-04-25 00:07:18 +0300

    Добавлен метод проверки корректности вводимого выражения в класс ParserExt

---
+++ Add_Class_To_Parse/Add_Class_To_Parse/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Add_Class_To_Parse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while(true)
+            {
+                //Создание экземпляра класса Parser
+                ParserExt obj = new ParserExt();
+                obj.GetExpression();
+                obj.ShowResult();
+            }
+        }
+    }
+}
